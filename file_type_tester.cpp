92c9e714692a1d4b49e788ba63294ea97479554f
Pavle Gvozdenovic <pavlegvozdenovic9@gmail.com>
2026-03-03 18:39:49 +0100

    added restriction on file types other than regular files, directories or symbolic links

--- file_type_tester.cpp
+++ file_type_tester.cpp
@@ -10,6 +10,9 @@ using std::string;
 using std::filesystem::directory_iterator;
 using std::filesystem::path;
 using std::filesystem::is_directory;
+using std::filesystem::is_regular_file;
+using std::filesystem::is_symlink;
+using std::filesystem::read_symlink;
 using std::ifstream;
 using std::filesystem::filesystem_error;
 using std::vector;
@@ -46,6 +49,20 @@ struct FileInfo{
 
 };
 
+bool is_valid_file(const string &file_path){
+
+    if(is_symlink(file_path))
+        return is_valid_file(read_symlink(file_path));
+    
+    
+
+    else if(is_regular_file(file_path) || is_directory(file_path))   
+        return true;
+
+    return false;
+
+}
+
 
 
 
@@ -59,22 +76,25 @@ void check_type(const path &directory_name, const FileInfo &fileinfo, const Scan
 
     for(const auto &file : it){
         try{
-        if(scan_options.recursive && is_directory(file.path()))
-            check_type(file.path(), fileinfo, scan_options);
-        ifstream ifile{file.path().string(), std::ios::binary};
-        if(ifile.is_open()){
-            long long magic_number {0};
-            ifile.read(reinterpret_cast<char*>(&magic_number), fileinfo.signature_length);
+            if(!is_valid_file(file.path().string()))
+                continue;
+                
+            if(scan_options.recursive && is_directory(file.path()))
+                check_type(file.path(), fileinfo, scan_options);
+            ifstream ifile{file.path().string(), std::ios::binary};
+            if(ifile.is_open()){
+                long long magic_number {0};
+                ifile.read(reinterpret_cast<char*>(&magic_number), fileinfo.signature_length);
 
-             if(magic_number == fileinfo.filetype){
+            if(magic_number == fileinfo.filetype){
                 cout<<file.path().string()<<endl;
             }
 
             ifile.close();
                 
         }
-        else if(scan_options.verbose && !is_directory(file.path())){
-            cout<<"Failed to open file: "<<file.path().generic_string()<<endl;
+            else if(scan_options.verbose && !is_directory(file.path())){
+                cout<<"Failed to open file: "<<file.path().generic_string()<<endl;
         }}
 
     catch(const filesystem_error &fse){
